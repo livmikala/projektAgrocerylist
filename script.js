3050c7509a55ffe07650b62cdcf3cbbe46d8240d
livmikala <livgun01@iba.dk>
2025-09-22 13:22:20 +0200

    variabler + array tilføjet

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+//Variabler
+
 const itemName = document.getElementById("itemName");
 const itemPrice = document.getElementById("itemPrice");
 const addBtn = document.getElementById("addBtn");
 const sortPriceBtn = document.getElementById("sortPriceBtn");
 const sortNameBtn = document.getElementById("sortNameBtn");
+const itemList = document.getElementById("itemList");
+const totalPrice = document.getElementById("totalPrice");
+
+let item = []; //liste kommer her i array'et efterhånden som varer bliver tilføjet
