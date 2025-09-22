fc7ec8d6cb2377aee87e2461928cd9604df10db4
livmikala <livgun01@iba.dk>
2025-09-22 13:32:47 +0200

    eventlistener tilføjet

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const itemList = document.getElementById("itemList");
 const totalPrice = document.getElementById("totalPrice");
 
 let item = []; //liste kommer her i array'et efterhånden som varer bliver tilføjet
+
+addBtn.addEventListener("click", () => {
+    const name = itemName.value.trim();
+    const price = parseFloat(itemPrice.value);
+
+    if (name && !isNaN(price)) {
+        itemList.push({ navn: name, pris: price});
+        itemName.value = "";
+        itemPrice.value = "";
+    } else {
+        alert("Varer skal være med bogstaver og pris med tal")
+    }
+});
