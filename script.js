3f4ed5f254aee14bab0393e2ba72ab8a8fe027c4
livmikala <livgun01@iba.dk>
2025-09-22 10:52:19 +0200

    variabler tilføjer

---
+++ script.js
@@ -0,0 +1,5 @@
+const itemName = document.getElementById("itemName");
+const itemPrice = document.getElementById("itemPrice");
+const addBtn = document.getElementById("addBtn");
+const sortPriceBtn = document.getElementById("sortPriceBtn");
+const sortNameBtn = document.getElementById("sortNameBtn");
